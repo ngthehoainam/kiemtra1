3356270ec272c6a37f56efaf14eb291e33f43a3f
ngthehoainam <nthnam.dev@gmail.com>
2021-11-19 11:59:15 +0700

    Initial commit

---
+++ assets/main.js
@@ -0,0 +1,12 @@
+// var title = 'Học Javascript tại F8';
+// function run(title) {
+//     language = title.slice(4, 14);
+//     rest = title.slice(15, 20);
+//     return `Language is ${language} and rest is ${rest}`;
+// }
+
+var title = 'Học Javascript tại F8';
+language = title.slice(4, 14);
+console.log(language);
+rest = title.slice(15, 21);
+console.log(rest);
